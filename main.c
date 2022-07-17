582321372212fcc5fa1aa7675ddb92e3c3926e63
atchoglogilbert <92089944+atchoglogilbert@users.noreply.github.com>
2022-07-17 19:58:35 +0200

    Main code
    The main code to run to get the factorial of a number, which is initially 5 and can be changed.
    
    Code checked by Norminette.

--- main.c
+++ main.c
@@ -1,7 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: katchogl <katchogl@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/17 19:38:41 by katchogl          #+#    #+#             */
+/*   Updated: 2022/07/17 19:38:41 by katchogl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 
-int main()
+int		fact(int n);
+
+int	main(void)
 {
-	printf ("hello world");
+	int		n;
+	int		factorial;
+
+	n = 5;
+	factorial = fact (n);
+	printf ("factorial of %d is %d", n, factorial);
 	return (0);
 }
+
+int	fact(int n)
+{
+	if (n > 1)
+	{
+		return (n * fact (n - 1));
+	}
+	else
+	{
+		return (1);
+	}
+	return (0);
+}
+// end
